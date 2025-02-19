7a62fd8e5943b487307db1a90e8bb9cc656d6130
Luka Prebil Grintal <pgluka7@gmail.com>
2025-02-19 19:36:09 +0100

    Cleanup user routes

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,25 +1,27 @@
-import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Query } from "@nestjs/common";
+import { CreateUserDto, User, UserDto } from "./User.entity";
 import { UserService } from "./user.service";
-import { User, CreateUserDto, UserDto } from "./User.entity";
 
 @Controller("users")
 export class UsersController {
   constructor(private userService: UserService) {}
 
-  @Get("by-id") // 1aca4117-3ab0-4dd6-8082-4717d4fface8
-  async getUserById(@Param("id") id: string): Promise<UserDto> {
+  @Get()
+  async getUserById(@Query("id") id: string): Promise<UserDto> {
+    console.log("Getting user with id: ", id);
     const user = await this.userService.getUserById(id);
     return user;
   }
 
-  @Post("create")
-  async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
-    // TODO return DTO
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto): Promise<UserDto> {
+    console.log("Creating user with email: ", createUserDto.email);
     return await this.userService.createUser(createUserDto.email);
   }
 
-  @Delete("delete")
-  async deleteUser(@Param("id") id: string): Promise<string> {
-    return await this.userService.softDeleteUser(id);
+  @Delete()
+  async deleteUser(@Body() body: { id: string }): Promise<string> {
+    console.log("Deleting user with id: ", body.id);
+    return await this.userService.softDeleteUser(body.id);
   }
 }
