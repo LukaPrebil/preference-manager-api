c5dae5c77daac086c1a6205700a343d720ca7b34
Luka Prebil Grintal <pgluka7@gmail.com>
2025-02-19 18:21:48 +0100

    Validate event insertion only for existing users

--- src/changeEvents/changeEvent.module.ts
+++ src/changeEvents/changeEvent.module.ts
@@ -3,9 +3,10 @@ import { ChangeEventService } from "./changeEvent.service";
 import { ChangeEventController } from "./changeEvent.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ChangeEvent } from "./ChangeEvent.entity";
+import { User } from "../users/User.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ChangeEvent])],
+  imports: [TypeOrmModule.forFeature([ChangeEvent, User])],
   controllers: [ChangeEventController],
   providers: [ChangeEventService],
 })
--- src/changeEvents/changeEvent.service.ts
+++ src/changeEvents/changeEvent.service.ts
@@ -1,25 +1,34 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ChangeEvent, ChangeEventType } from "./ChangeEvent.entity";
-import { Repository } from "typeorm";
+import { In, Repository } from "typeorm";
 import { User } from "src/users/User.entity";
 
 @Injectable()
 export class ChangeEventService {
   constructor(
-    @InjectRepository(ChangeEvent) 
-    private readonly changeEventRepo: Repository<ChangeEvent>
+    @InjectRepository(ChangeEvent)
+    private readonly changeEventRepo: Repository<ChangeEvent>,
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
   ) {}
 
   async insertChangeEvent(
     userId: string,
     eventType: ChangeEventType,
-    payload: Record<string, unknown>
+    payload: Record<string, unknown>,
   ): Promise<ChangeEvent> {
+    // First make sure we are creating an event for a valid user
+    const userExists = await this.userRepo.exists({ where: { id: userId } }); // TODO check soft delete when implemented
+    if (!userExists) {
+      throw new HttpException(`User with id ${userId} does not exist`, HttpStatus.NOT_FOUND);
+    }
+    
     const changeEvent = new ChangeEvent();
     changeEvent.user = { id: userId } as User;
     changeEvent.event_type = eventType;
     changeEvent.payload = payload;
+    
     return this.changeEventRepo.save(changeEvent);
   }
 
