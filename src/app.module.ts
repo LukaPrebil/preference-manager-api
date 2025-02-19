c382d505f5adf38d3d72688f80c8f1acb4b2852c
Luka Prebil Grintal <pgluka7@gmail.com>
2025-02-19 16:57:44 +0100

    Register the user routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,17 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { connectionConfig } from "./ormconfig";
 import { User } from "./users/User.entity";
+import { ChangeEvent } from "./changeEvents/ChangeEvent.entity";
+import { UserModule } from "./users/user.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot(), // Loads .env file
     TypeOrmModule.forRoot({
       ...connectionConfig,
-      entities: [User],
+      entities: [User, ChangeEvent],
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
