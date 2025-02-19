387d01f1af91c524c8597dbae05093ce257da80e
Luka Prebil Grintal <pgluka7@gmail.com>
2025-02-19 11:29:26 +0100

    Migrations setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { connectionConfig } from './ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './users/User.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // Loads .env file
+    TypeOrmModule.forRoot({
+      ...connectionConfig,
+      entities: [User],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
