a4e4a7c17f6afba2820cfaecf4b6eb787a379978
Luka Prebil Grintal <pgluka7@gmail.com>
2025-02-21 10:39:08 +0100

    rename events route

--- src/changeEvents/changeEvent.controller.ts
+++ src/changeEvents/changeEvent.controller.ts
@@ -2,11 +2,11 @@ import { Body, Controller, Post } from "@nestjs/common";
 import { CreateChangeEventDto } from "./ChangeEvent.entity";
 import { ChangeEventService } from "./changeEvent.service";
 
-@Controller("change-events")
+@Controller("events")
 export class ChangeEventController {
   constructor(private readonly changeEventService: ChangeEventService) {}
 
-  @Post("create")
+  @Post()
   async createChangeEvent(@Body() body: CreateChangeEventDto) {
     return this.changeEventService.insertChangeEvent(body.userId, body.eventType, body.payload);
   }
