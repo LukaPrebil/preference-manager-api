6f409b477016ab393af265c7f1bd89445164aa17
Luka Prebil Grintal <pgluka7@gmail.com>
2025-02-21 14:55:38 +0100

    optimize mergeSubscriptionConsentChangeEvents

--- src/changeEvents/subscriptionEvent.helpers.ts
+++ src/changeEvents/subscriptionEvent.helpers.ts
@@ -1,22 +1,19 @@
 import { ChangeEvent } from "./ChangeEvent.entity";
 import { ChangeEventType, SubscriptionType } from "./payload.dto";
 
-export type SubscriptionChange = {
-  email_notifications: boolean;
-  sms_notifications: boolean;
-};
-
-export type SubscriptionChangeEvent = ChangeEvent & {
-  eventType: (typeof ChangeEventType)["NOTIFICATION_PREFERENCE_CHANGE"];
-  payload: Partial<SubscriptionChange>;
-};
-
 /**
  * This is a type that represents the state of the user's subscription preferences.
  * It is an array of objects, each object representing a subscription type (email or sms) and whether it is enabled or not.
  * There can be only one object for each subscription type. The SubscriptionType union is used to define the possible subscription types.
  */
 export type SubscriptionState = { [K in SubscriptionType]: { id: K; enabled: boolean } }[SubscriptionType][];
+
+export type SubscriptionChangeEvent = ChangeEvent & {
+  event_type: (typeof ChangeEventType)["NOTIFICATION_PREFERENCE_CHANGE"];
+  payload: {
+    consents: Partial<SubscriptionState>;
+  };
+};
 
 /**
  * Takes an array of subscription change events and merges them into a single subscription state.
@@ -28,18 +25,16 @@ export function mergeSubscriptionConsentChangeEvents(
 ): SubscriptionState {
   const reducedChangeEvents = subscriptionChangeEvents.reduce(
     (state, changeEvent) => {
-      const payload = changeEvent.payload as Partial<SubscriptionChange>;
-
-      return {
-        email_notifications: payload.email_notifications ?? state.email_notifications,
-        sms_notifications: payload.sms_notifications ?? state.sms_notifications,
-      };
+      for (const c of changeEvent.payload.consents) {
+        state[c.id] = c.enabled;
+      }
+      return state;
     },
-    {
-      email_notifications: false,
-      sms_notifications: false,
-    },
+    {} as { [K in SubscriptionType]: boolean },
   );
 
-  return Object.entries(reducedChangeEvents).map(([id, enabled]) => ({ id: id as SubscriptionType, enabled }));
+  return [
+    { id: "email_notifications", enabled: reducedChangeEvents.email_notifications },
+    { id: "sms_notifications", enabled: reducedChangeEvents.sms_notifications },
+  ];
 }
